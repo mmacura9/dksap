408218174ca8a05502eff57d17d0b6d953aacc62
mmacura9 <marko.macura9@gmail.com>
2025-01-15 22:00:29 +0100

    Add metamask connection

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <MetaMaskProvider
+      debug={false}
+      sdkOptions={{
+        dappMetadata: {
+          name: "Example React Dapp",
+          url: window.location.href,
+        },
+        infuraAPIKey: process.env.INFURA_API_KEY,
+        // Other options
+      }}
+    >
+      <App />
+    </MetaMaskProvider>
   </React.StrictMode>
 );
